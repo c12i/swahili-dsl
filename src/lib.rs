dd6caccb5df3f0e3fb42d49882cd01d513b47529
Collins Muriuki <murerwacollins@gmail.com>
2020-10-05 21:15:46 +0300

    vars and print statement

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #[macro_export]
 macro_rules! swh {
-    (andika($expression:expr)) => {{
+    (andika($expression:expr)) => {
         println!("{:?}", $expression);
-    }};    
+    };    
 
-    (wacha $name:ident = $expression:expr) => {{
+    (wacha $name:ident = $expression:expr) => {
+        #[allow(unused_mut)]
         let mut $name = $expression;
-    
-    }};
+    };
 }
 
 #[cfg(test)]
@@ -16,7 +16,18 @@ mod tests {
 
     #[test]
     fn andika() {
-        swh!(andika("Habari Duinia"));
+        swh!(
+            andika("Habari Duinia")
+        );
         assert!(true);
+    }
+
+    #[test]
+    fn wacha() {
+        swh!(
+            wacha jina = 2020
+        );
+        jina += 10;
+        assert_eq!(jina, 2030);
     }
 }
